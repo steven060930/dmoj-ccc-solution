42fdd8798c446e974a74c223dc90e4ae166593c1
steven060930 <84993331+steven060930@users.noreply.github.com>
2021-11-18 19:53:09 -0500

    Update ccc04s1.cpp

--- 2004/ccc04s1.cpp
+++ 2004/ccc04s1.cpp
@@ -1,1 +1,126 @@
+#include "bits/stdc++.h"
+using namespace std;
 
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mpr make_pair
+#define pb push_back
+#define scan(x) { while((x=getchar())<'0'); for (x-='0'; '0'<= (_=getchar()); x = (x<<3)+(x<<1)+_-'0');} while(0)
+char _;
+#define vt vector
+typedef unsigned long long ull;	 typedef long long ll;	typedef long double ld;
+const ll INF = 0x3f3f3f3f;	const ll LLINF = 0x3f3f3f3f3f3f3f3f;	const ll MOD = 1e9 +7;
+template <typename C> void UNIQUE(vector<C> &v) {sort(all(v)); v.resize(unique(all(v)) - v.begin());}
+template <typename T> int sz(T &v) { return (int)(v.size()); }
+
+template<class T> void read(T& x) {
+	cin >> x;
+} 
+void read(double &d) {
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(ld &d) {
+	string t;
+	read(t);
+	d = stold(t);
+}
+template<class H, class... T> void read(H& h, T&...t) {
+	read(h);
+	read(t...);
+}
+template<class A> void read(vector<A> &x) {
+	for (auto &a : x) { read(a); }
+}
+
+string to_string(char c) {
+	return string(1, c);
+}
+string to_string(bool b) {
+	return b?"true":"false";
+}
+string to_string(const char* s) {
+	return string(s);
+}
+string to_string(string s) {
+	return s;
+}
+string to_string(vt<bool> v) {
+	string res;
+	for (int i=0; i < (int)v.size(); i++)
+		res+=char('0'+v[i]);
+	return res;
+}
+
+template<class T> string to_string(T v) {
+    bool f=1;
+    string res;
+    for (auto &x :v) {
+		if(!f)
+			res+=' ';
+		f=0;
+		res+=to_string(x);
+    }
+    return res;
+}
+
+template<class A> void write(A x) {
+	cout << to_string(x);
+}
+template<class H, class... T> void write(const H& h, const T&... t) { 
+	write(h);
+	write(t...);
+}
+void print() {
+	write("\n");
+}
+template<class H, class... T> void print(const H& h, const T&... t) { 
+	write(h);
+	if(sizeof...(t))
+		write(' ');
+	print(t...);
+}
+
+bool cmp(string s, string t) {
+	string long_s, short_s;
+	int x = sz(s), y= sz(t);
+	if (x == y) return s == t;
+	else if (x > y) {
+		long_s = s;
+		short_s = t;
+	}
+	else {
+		long_s = t;
+		short_s = s;
+	}
+	int c = min(x, y), d = max(x, y);
+	return (long_s.substr(0, c) == short_s) || (long_s.substr(d-c, c) == short_s);
+	return false;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n; read(n);
+	for (int i = 0; i < n; i++) {
+		vt<string> v;
+		bool flag = true;
+		for (int j = 0; j < 3; j++) {
+			string str; read(str);
+			v.pb(str);
+		}
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				if (x != y && cmp(v[x], v[y])) {
+					flag = false;
+					break;
+				}
+			}
+		}
+		if (flag) print("Yes");
+		else print("No");
+	}
+	return 0;
+}
