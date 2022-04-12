a395b2ea43a210d6cac1e9eb67fc7bc4f1635bc0
steven060930 <84993331+steven060930@users.noreply.github.com>
2022-04-11 21:11:06 -0400

    Create ccc16s2.cpp

---
+++ 2016/ccc16s2.cpp
@@ -0,0 +1,93 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define mpr make_pair
+#define pb push_back
+#define vt vector
+typedef unsigned long long ull;	 typedef long long ll;	typedef long double ld;
+const ll INF = 0x3f3f3f3f;	const ll LLINF = 0x3f3f3f3f3f3f3f3f;	const ll MOD = 1e9 +7;
+template <typename C> void UNIQUE(vector<C> &v) {sort(all(v)); v.resize(unique(all(v)) - v.begin());}
+template <typename T> int sz(T &v) { return (int)(v.size()); }
+
+template<class T> void read(T& x) { cin >> x; } 
+void read(double &d) {
+	string t; read(t);
+	d = stod(t);
+}
+void read(ld &d) {
+	string t; read(t);
+	d = stold(t);
+}
+template<class H, class... T> void read(H& h, T&...t) {
+	read(h); read(t...);
+}
+template<class A> void read(vector<A> &x) {
+	for (auto &a : x) { read(a); }
+}
+
+string to_string(char c) { return string(1, c); }
+string to_string(bool b) { return b?"true":"false"; }
+string to_string(const char* s) { return string(s); }
+string to_string(string s) { return s; }
+string to_string(vt<bool> v) {
+	string res;
+	for (int i=0; i < (int)v.size(); i++)
+		res+=char('0'+v[i]);
+	return res;
+}
+template<class T> string to_string(T v) {
+    bool f=1; string res;
+    for (auto &x :v) {
+		if(!f)
+			res+=' ';
+		f=0;
+		res+=to_string(x);
+    }
+    return res;
+}
+
+template<class A> void write(A x) { cout << to_string(x); }
+template<class H, class... T> void write(const H& h, const T&... t) { write(h); write(t...); }
+
+void print() { write("\n"); }
+template<class H, class... T> void print(const H& h, const T&... t) { 
+	write(h);
+	if(sizeof...(t))
+		write(' ');
+	print(t...);
+}
+
+int main() {
+	cin.tie(nullptr) -> sync_with_stdio(0);
+	int type; cin >> type;
+	if (type == 1) {
+		int n; cin >> n;
+		vt<int> dmoj(n), peg(n);
+		read(dmoj); read(peg);
+		sort(all(dmoj)); sort(all(peg));
+		ll ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans += max(dmoj[i], peg[i]);
+		}
+		print(ans);
+	}
+	else if (type == 2) {
+		int n; cin >> n;
+		vt<int> dmoj(n), peg(n);
+		read(dmoj); read(peg);
+		ll a1 = 0, a2 = 0;
+		sort(all(dmoj), greater<int>()); sort(all(peg));
+		for (int i = 0; i < n; i++) {
+			a1 += max(dmoj[i], peg[i]);
+		}
+		sort(all(peg),greater<int>()); sort(all(dmoj));
+		for (int i = 0 ; i <n; i++) {
+			a2 += max(dmoj[i], peg[i]);
+		}
+		print(max(a1, a2));
+	}
+	return 0;
+}
